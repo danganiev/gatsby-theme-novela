1fd928b65b9cea3ddb506c67834004696d6cc034
Dan Ganiev <dan@danganiev.me>
2021-04-05 16:01:44 +0300

    Removed url copy button

--- src/components/Navigation/Navigation.Header.tsx
+++ src/components/Navigation/Navigation.Header.tsx
@@ -48,39 +48,6 @@ const DarkModeToggle: React.FC<{}> = () => {
   );
 };
 
-const SharePageButton: React.FC<{}> = () => {
-  const [hasCopied, setHasCopied] = useState<boolean>(false);
-  const [colorMode] = useColorMode();
-  const isDark = colorMode === `dark`;
-  const fill = isDark ? "#fff" : "#000";
-
-  function copyToClipboardOnClick() {
-    if (hasCopied) return;
-
-    copyToClipboard(window.location.href);
-    setHasCopied(true);
-
-    setTimeout(() => {
-      setHasCopied(false);
-    }, 1000);
-  }
-
-  return (
-    <IconWrapper
-      isDark={isDark}
-      onClick={copyToClipboardOnClick}
-      data-a11y="false"
-      aria-label="Copy URL to clipboard"
-      title="Copy URL to clipboard"
-    >
-      <Icons.Link fill={fill} />
-      <ToolTip isDark={isDark} hasCopied={hasCopied}>
-        Copied
-      </ToolTip>
-    </IconWrapper>
-  );
-};
-
 const NavigationHeader: React.FC<{}> = () => {
   const [showBackArrow, setShowBackArrow] = useState<boolean>(false);
   const [previousPath, setPreviousPath] = useState<string>("/");
@@ -135,7 +102,6 @@ const NavigationHeader: React.FC<{}> = () => {
             </button>
           ) : (
             <>
-              <SharePageButton />
               <DarkModeToggle />
             </>
           )}
